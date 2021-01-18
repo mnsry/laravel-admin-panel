411cb6160609aac5627c8e5bdb5b2161dd1bcaf8
mansorygit <mansory.git@gmail.com>
2021-01-19 02:01:06 +0330

    Fake Product Dummy

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -18,28 +18,28 @@ class ProductSeeder extends Seeder
             'user_id' => 1,
             'title'   => 'گوشی موبایل سامسونگ S10',
             'body'    => 'توضیحات برای گوشی سامسونگ s10  و مشخصات',
-            'image'   => 'https://saymandigital.com/wp-content/uploads/2019/02/Samsung-Galaxy-S10-Dual-SIM.jpg'
+            'image'   => 'mansory.gif'
         ]);
 
         Product::create([
             'user_id' => 1,
             'title'   => 'گوشی موبایل آیفن 11',
             'body'    => 'توضیحات برای گوشی آیفن 11 و مشخصات',
-            'image'   => 'https://s.mobile.ir/Static/cache/prd/38655-iPhone_11_01_610_710.jpg'
+            'image'   => 'mansory.gif'
         ]);
 
         Product::create([
             'user_id' => 2,
             'title'   => 'لبتاب لنوو s540',
             'body'    => 'توضیحات برای لبتاب لنوو اس 540  و مشخصات',
-            'image'   => 'https://lotuslaptop.com/wp-content/uploads/2019/09/911dhIY2SCL._AC_SL1500_.jpg'
+            'image'   => 'mansory.gif'
         ]);
 
         Product::create([
             'user_id' => 2,
             'title'   => 'لبتاب مک بوک پرو',
             'body'    => 'توضیحات برای لبتاب مک بوک پرو و مشخصات',
-            'image'   => 'https://www.apple-nic.com/images/store/apple-macbook-pro-16-inch-2019-spacegray.jpg'
+            'image'   => 'mansory.gif'
         ]);
     }
 }
