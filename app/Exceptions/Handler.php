bfb6c60af49af85ced675136aa9de24c71b8f62c
mansorygit <mansory.git@gmail.com>
2021-01-18 23:45:30 +0330

    Custom Message Errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -41,20 +43,35 @@ class Handler extends ExceptionHandler
         });
     }
 
+    /**
+     * @note Custom Error, For Custom Message And Custom Http StatusCode
+     *
+     * @param $request
+     * @param Throwable $exception
+     * @return JsonResponse
+     * @throws Throwable
+     */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof ModelNotFoundException) {
-            return response()->json(['message' => 'همچین مسیری وجود ندارد!'], 404);
+        if ($exception instanceof AuthenticationException) {
+            return response()->json(['message' => 'احراز هویت نشدید!'], 401);
         }
+
         if ($exception instanceof AuthorizationException) {
             return response()->json(['message' => 'دسترسی وجود ندارد!'], 403);
         }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(['message' => 'همچین مسیری وجود ندارد!'], 404);
+        }
+
         if ($exception instanceof ValidationException) {
             return response()->json([
                 'message' => 'داده های داده شده نامعتبر است.',
                 'errors' => $exception->errors(),
             ], 422);
         }
+
         return parent::render($request, $exception);
     }
 }
