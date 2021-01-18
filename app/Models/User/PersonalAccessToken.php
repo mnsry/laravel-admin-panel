ff9f2b53b199c26cc7cf1513a4f286f7d3b1eb38
mansorygit <mansory.git@gmail.com>
2021-01-18 23:36:46 +0330

    Add Method For Relation and more

--- app/Models/User/PersonalAccessToken.php
+++ app/Models/User/PersonalAccessToken.php
@@ -6,5 +6,10 @@ use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
+    /**
+     * @note This Model Relation Many Token With One { @see User::tokens }
+     */
+
+    // Table Name For Create Token
     protected $table = 'user_personal_access_tokens';
 }
--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -2,37 +2,47 @@
 
 namespace App\Models\User;
 
-use App\Models\Product\Product;
-use App\Models\Role\Role;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Product\Product;
+use App\Models\Role\Role;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * @note This Model Relation Many User With Mane { @see Role::users() }
+     * @note This Model Relation One User With Many { @see Product::user() }
+     */
 
+    use HasApiTokens, Notifiable;
+
+    // Add Column, If Column Use InTo Form
     protected $fillable = ['name', 'family', 'avatar', 'mobile', 'email', 'password',];
 
+    // If Dont Send Column
     protected $hidden = ['password', 'remember_token',];
 
+    // Cast For Column
     protected $casts = [
         'email_verified_at' => 'date:d-M-y',
         'created_at' => 'datetime:D, d M Y H:i:s T',
     ];
 
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
+    // BrowseAdmin Middleware
     public function hasPermission($name)
     {
         $permissions = $this->roles->pluck('permissions')->flatten()->pluck('key')->toArray();
         return in_array($name, $permissions);
     }
 
+    // Relation Many User With Many Model Role
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    // Relation One User With Many Model Product
     public function products()
     {
         return $this->hasMany(Product::class);
